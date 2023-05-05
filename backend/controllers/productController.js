6bb1df17803f1a785b6fb0eb6511a5ca6504dc29
srinivasv662 <srinivasv662@gmail.com>
2023-05-05 18:41:18 +0530

    productController modified and apifeature added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -14,7 +15,8 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-    const products = await Product.find();
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+    const products = await apiFeature.query;
 
     res.status(200).json({
         success: true,
